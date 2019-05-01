955714bf2d7e51174d122b3c3b8213fa1896bbaa
Dominika Myszczynska <myszczynskad@gmail.com>
2019-05-01 22:00:46 +0100

    all colors added

--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -1,4 +1,5 @@
 var os = require('os');
+var colors = require('colors');
 var time = require('./time');
 
 function getOSinfo() {
@@ -12,12 +13,12 @@ function getOSinfo() {
     var cpu = os.cpus()[0].model;
     var uptime = os.uptime();
     var userInfo = os.userInfo();
-    console.log('System:', type);
-    console.log('Release:', release);
-    console.log('CPU model:', cpu);
-    console.log('Uptime: ~', time.format(uptime));
-    console.log('User name:', userInfo.username);
-    console.log('Home dir:', userInfo.homedir);
+    console.log('System:'.green, type);
+    console.log('Release:'.red, release);
+    console.log('CPU model:'.blue, cpu);
+    console.log('Uptime: ~'.yellow, time.format(uptime));
+    console.log('User name:'.rainbow, userInfo.username);
+    console.log('Home dir:'.zebra, userInfo.homedir);
 }
 
 exports.print = getOSinfo;
